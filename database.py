923626e7d6c2f0fbda3403efaa0205066406efec
lai931 <elden.lai@my.rctc.edu>
2020-02-04 09:55:15 -0600

    completed assignment

--- database.py
+++ database.py
@@ -2,19 +2,20 @@ import pymysql.cursors
 
 # Connect to the database
 connection = pymysql.connect(host='mrbartucz.com',
-                             user='CS485',
-                             password='WinonaState',
-                             db='CS485',
+                             user='gw2246fx',
+                             password='9Astronaut',
+                             db='gw2246fx_university',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
 try:
     with connection.cursor() as cursor:
         # Select all Students
-        sql = "SELECT * from Students"
+        user_input = input("enter a student name: ")
+        sql = "SELECT * from Student where firstname = %s"
         
         # execute the SQL command
-        cursor.execute(sql)
+        cursor.execute(sql, (user_input))
         
         # get the results
         for result in cursor:
